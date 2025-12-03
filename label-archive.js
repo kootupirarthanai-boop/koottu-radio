86a2aad8f5c308770174ae8d95a937e83f52b650
kootupirarthanai-boop <kootupirarthanai@gmail.com>
2025-12-03 14:12:32 -0600

    Update label-archive.js

--- label-archive.js
+++ label-archive.js
@@ -2,7 +2,7 @@
 
 (function () {
   const BLOG_URL = "https://koottuppirarthanai.blogspot.com";
-  const LABELS = ["31-08-2025 - திருஅண்ணாமலை", "2-11-2025- திருவண்ணாமலை", "05-10-2025 - மதுரை", "27-7-2025  - பாபநாசம்"];
+  const LABELS = ["31-08-2025 - திருஅண்ணாமலை", "2-11-2025 - திருவண்ணாமலை", "05-10-2025 - மதுரை", "27-7-2025  - பாபநாசம்"];
   const MAX_POSTS = 500; // enough for whole blog
 
   function toggle(id) {
