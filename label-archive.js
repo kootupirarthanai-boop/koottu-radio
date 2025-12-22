470237cd400ac9a5b531025b28747d90ad1eea43
kootupirarthanai-boop <kootupirarthanai@gmail.com>
2025-12-22 14:47:15 -0600

    Update label-archive.js

--- label-archive.js
+++ label-archive.js
@@ -19,6 +19,12 @@
     },
     {
       // EXACT Blogger label text (no spaces before/after last dash)
+      key: "19-12-2025-இலங்கை",
+      // Nicely formatted title to show in the widget
+      title: "19-12-2025-இலங்கை"
+    },
+    {
+      // EXACT Blogger label text (no spaces before/after last dash)
       key: "07-12-2025-மதுரை",
       // Nicely formatted title to show in the widget
       title: "07-12-2025 - மதுரை"
